830e39d4c12dda104e51678dbe3b14248342f4aa
Alla Berestova <alla.chigvintseva28@gmail.com>
2025-02-27 13:27:05 +0300

    second exercise

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
-import React from 'react';
-import { useGetTodoList } from './hooks/useGetTodoList';
+import React, { useState } from 'react';
+import { useLocalStorage } from './hooks/useLocalStorage';
 import './App.css';
 
 function App() {
-  const completed = true
-  const {tasks, loading} = useGetTodoList(completed)
+  const [data, setData] = useState(null)
+  const [localStorageKey, setLocalStorageKey] = useState('')
+  const [localStorageValue, setLocalStorageValue] = useState('')
+  const {setLocalStorage, getLocalStorage, removeLocalStorage} = useLocalStorage()
+
+  const onGetData = () => {
+    const data = getLocalStorage(localStorageKey)
+    setData(data)
+    console.log(`Данные получены из localStorage по ключу "${localStorageKey}":`, data)
+  }
+
+  const onSetData = () => {
+    setLocalStorage(localStorageKey, localStorageValue)
+    console.log(`Данные записаны в localStorage по ключу "${localStorageKey}":`, localStorageValue)
+  }
+
+  const onRemoveData = () => {
+    removeLocalStorage(localStorageKey)
+    console.log(`Данные удалены из localStorage по ключу "${localStorageKey}"`)
+  }
   return (
     <div className="App">
-      <h1>Выполненные задачи</h1>
-      {
-        loading && <div>Loading...</div>
-      }
-      {
-      !loading && tasks.map(task => <div key={task.id}>{task.title}</div>)
-      }
+      <h1>Пример кастомного хука 2</h1>
+      <form>
+        {data && <div>Данные из localStorage: {data}</div>}
+        <input
+          type='text'
+          name='key'
+          value={localStorageKey}
+          onChange={(e) => setLocalStorageKey(e.target.value)}
+          placeholder='localStorage key'
+        />
+        <br/>
+        <br/>
+        <input
+          type='text'
+          name='value'
+          value={localStorageValue}
+          onChange={(e) => setLocalStorageValue(e.target.value)}
+          placeholder='localStorage value'
+        />
+        <br/>
+        <br/>
+        <div onClick={onSetData}>Записать данные</div>
+        <div onClick={onGetData}>Получить данные</div>
+        <div onClick={onRemoveData}>Удалить данные</div>
+      </form>
+      
     </div>
   );
 }
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,21 @@
+export const useLocalStorage = () => {
+    const setLocalStorage = (key, value) => {
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+    const getLocalStorage = (key) => {
+        try{
+            return JSON.parse(localStorage.getItem(key))
+        } catch (e) {
+            throw new Error(e)
+        }
+    }
+    const removeLocalStorage = (key) => {
+        localStorage.removeItem(key)
+    }
+
+    return {
+        setLocalStorage,
+        getLocalStorage,
+        removeLocalStorage
+    }
+}
